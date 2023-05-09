499002b489eddc1dc7087be32c80f777cbf34823
Bryan-014 <bryangustavo201904@gmail.com>
2023-05-09 17:12:50 -0300

    update desnig and add shortcut

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var gameMatriz = loadInitMatriz();
 var vezDeVar = loadInitPlayer();
 var placar = placInit();
 
+changeFocusPlayer();
+
 function loadInitMatriz() {
   return [
     [0, 0, 0],
@@ -26,7 +28,7 @@ function playIn(Locale, position) {
   if (document.querySelector('#' + Locale + position).value == '.') {
     document.querySelector('#' + Locale + position).value = vezDeVar;
     document.querySelector('#' + Locale + position).style.color =
-      vezDeVar == 'X' ? '#f00' : '#00f';
+      vezDeVar == 'X' ? '#911d1d' : '#1b1d8f';
 
     switch (Locale) {
       case 'top':
@@ -62,13 +64,17 @@ function playIn(Locale, position) {
       default:
         break;
     }
-
     vezDeVar = vezDeVar == 'X' ? 'O' : 'X';
-    document.querySelector('#vezDe').innerHTML = vezDeVar;
 
     if (winCheck != 0) {
       updatePlac();
-      restart(winCheck == 1 ? 'X' : 'O');
+      if (winCheck == 3) {
+        let rand = Math.floor(Math.random() * 2);
+        vezDeVar = rand == 0 ? 'X' : 'O';
+        restart(vezDeVar);
+      } else {
+        restart(winCheck == 1 ? 'X' : 'O');
+      }
       if (document.querySelector('#mode').value != 'loc' && vezDeVar == 'O') {
         botTurn();
       }
@@ -77,6 +83,8 @@ function playIn(Locale, position) {
         botTurn();
       }
     }
+
+    changeFocusPlayer();
   }
 }
 
@@ -128,26 +136,27 @@ function updatePlac() {
 function restart(playerInit) {
   gameMatriz = loadInitMatriz();
   vezDeVar = playerInit;
-  document.querySelector('#vezDe').innerHTML = vezDeVar;
+
+  changeFocusPlayer();
 
   document.querySelector('#top1').value = '.';
-  document.querySelector('#top1').style.color = '#bababa';
+  document.querySelector('#top1').style.color = '#21a652';
   document.querySelector('#top2').value = '.';
-  document.querySelector('#top2').style.color = '#bababa';
+  document.querySelector('#top2').style.color = '#21a652';
   document.querySelector('#top3').value = '.';
-  document.querySelector('#top3').style.color = '#bababa';
+  document.querySelector('#top3').style.color = '#21a652';
   document.querySelector('#middle1').value = '.';
-  document.querySelector('#middle1').style.color = '#bababa';
+  document.querySelector('#middle1').style.color = '#21a652';
   document.querySelector('#middle2').value = '.';
-  document.querySelector('#middle2').style.color = '#bababa';
+  document.querySelector('#middle2').style.color = '#21a652';
   document.querySelector('#middle3').value = '.';
-  document.querySelector('#middle3').style.color = '#bababa';
+  document.querySelector('#middle3').style.color = '#21a652';
   document.querySelector('#bottom1').value = '.';
-  document.querySelector('#bottom1').style.color = '#bababa';
+  document.querySelector('#bottom1').style.color = '#21a652';
   document.querySelector('#bottom2').value = '.';
-  document.querySelector('#bottom2').style.color = '#bababa';
+  document.querySelector('#bottom2').style.color = '#21a652';
   document.querySelector('#bottom3').value = '.';
-  document.querySelector('#bottom3').style.color = '#bababa';
+  document.querySelector('#bottom3').style.color = '#21a652';
 }
 
 document.querySelector('#mode').addEventListener('change', () => {
@@ -155,3 +164,46 @@ document.querySelector('#mode').addEventListener('change', () => {
   placar = placInit();
   updatePlac();
 });
+
+document.addEventListener('keydown', e => {
+  switch (e.keyCode) {
+    case 97:
+      playIn('bottom', '1');
+      break;
+    case 98:
+      playIn('bottom', '2');
+      break;
+    case 99:
+      playIn('bottom', '3');
+      break;
+    case 100:
+      playIn('middle', '1');
+      break;
+    case 101:
+      playIn('middle', '2');
+      break;
+    case 102:
+      playIn('middle', '3');
+      break;
+    case 103:
+      playIn('top', '1');
+      break;
+    case 104:
+      playIn('top', '2');
+      break;
+    case 105:
+      playIn('top', '3');
+      break;
+    default:
+      break;
+  }
+});
+
+function changeFocusPlayer() {
+  document.querySelector('.plac.red').style.border = '2px solid #000';
+  document.querySelector('.plac.blue').style.border = '2px solid #000';
+
+  let element = vezDeVar == 'X' ? 'red' : 'blue';
+
+  document.querySelector('.plac.' + element).style.border = '2px solid #14e32d';
+}
